9373d40293609e78f88941e177b93ab319650b53
BiancaReset <130721122+BiancaReset@users.noreply.github.com>
2023-08-30 02:16:01 +0000

    29 ago

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,70 +1,123 @@
 import React, { useState, useEffect } from 'react';
 
-const API_URL = 'https://playground.4geeks.com/apis/fake/todos/user/BiancaReset';
+const TodoApp = () => {
+  const [inputText, setInputText] = useState("");
+  const [todosList, setTodosList] = useState([]);
 
-export default function TodoList() {
-  async function fetchApi() {
-    let headers = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }
-    let response = await fetch(API_URL, headers);
-    let data = await response.json();
-    console.log(data);
-  }
+  useEffect(() => {
+    bringTodos()
+    createTodo()
+  }, []);
 
-  async function createUser() {
-    let headers = {
+
+  const createTodo = () => {
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/BiancaReset", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
       body: JSON.stringify([]),
-    }
-    let create = await fetch(API_URL, headers);
-    let newUser = await create.json();
-    console.log(newUser);
-  }
-
-  async function updateTodo() {
-    let headers = {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify([
-        { label: "Make the bed", done: false },
-        { label: "Walk the dog", done: false },
-        { label: "Do the replits", done: false }
-      ]),
-    };
-    let update = await fetch(API_URL, headers);
-    let data = await update.json();
-    console.log(data)
-    console.log("hola");
-  }
-
-  async function deleteTodo() {
-    let headers = {
-      method: "DELETE",
       headers: {
         "Content-Type": "application/json"
       }
-    };
-    let deleteTask = await fetch(API_URL, headers);
-    let newTodo = await deleteTask.json();
-
-    console.log(newTodo);
+    }).then(response => response.json)
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
   }
-  deleteTodo();
+  const addTodo = () => {
+    let task = { label: inputText, done: false }
+    let pendingTodos = [...todosList, task]
+    setTodosList(pendingTodos)
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/BiancaReset', {
+      method: "PUT",
+      body: JSON.stringify(todosList),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error("These has been an error");
+        }
+        return resp.json();
+      })
+      .then((data) => {
+        setTodosList(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  const bringTodos = () => {
+    fetch(
+      "https://playground.4geeks.com/apis/fake/todos/user/BiancaReset",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then(resp => resp.json())
+      .then(data => setTodosList(data))
+      .catch(error => console.log(error))
+  }
+
+  const deleteTodo = () => {
+
+    fetch(
+      "https://playground.4geeks.com/apis/fake/todos/user/BiancaReset/",
+      {
+        method: "DELETE",
+      }
+    ).then(() => {
+    });
+  };
+
+  const deleteAllTodos = () => {
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/BiancaReset", {
+      method: "DELETE",
+
+    }).then(() => {
+      setTodosList([]);
+    });
+  };
 
 
   return (
-    <>
-    </>
-  )
-}
+    <div className="text-center mt-5 container">
+      <h1>Todo list</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          addTodo();
+        }}
+      >
+        <input
+          className="form-control form-control-lg my-3 elinput"
+          placeholder="Add todo"
+          onChange={(event) => setInputText(event.target.value)}
+          value={inputText}
+        />
+        <button type="submit" className="btn btn-primary">
+          Add Todo
+        </button>
+      </form>
+      <ul className="list-group row">
+        {todosList.length > 0 && todosList.map((item) => (
+          <li className="list-group-item text-left elementos" key={item._id}>
+            <span>{item.label}</span>
+            <button
+              className="btn btn-danger eliminador float-right"
+              onClick={() => deleteTodo(item._id)}
+            >
+              <span>X</span>
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="pendientes">{todosList.length} pending</div>
+      <button className="btn btn-danger" onClick={deleteAllTodos}>
+        Delete Todo List
+      </button>
+    </div>
+  );
+};
 
-
+export default TodoApp;
